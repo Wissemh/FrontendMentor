1ec7049d812532a86f64da043d36d48dc4c334d7
unknown <wissemhssan227@gmail.com>
2022-02-09 00:30:24 +0100

    My First Project

---
+++ project.js
@@ -0,0 +1,21 @@
+alert("Please note that this project only has a desktop and a mobile version. No special design for meduim screens.");
+$("#id1").click(function() {
+    $("#id1").toggleClass("selected");    
+
+});
+$("#id2").click(function() {
+    $("#id2").toggleClass("selected");    
+
+});
+$("#id3").click(function() {
+    $("#id3").toggleClass("selected");    
+
+});
+$("#id4").click(function() {
+    $("#id4").toggleClass("selected");    
+
+});
+$("#id5").click(function() {
+    $("#id5").toggleClass("selected");    
+
+});
